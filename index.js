2ee60886f2dcc83e78d50f8ffe31a265faf949db
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-07-06 23:27:04 +0300

    add try catch

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const contacts = require("./contacts");
 const { program } = require("commander");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-	switch (action) {
-		case "list":
-			const allContacts = await contacts.listContacts();
-			return console.table(allContacts);
+	try {
+		switch (action) {
+			case "list":
+				const allContacts = await contacts.listContacts();
+				return console.table(allContacts);
 
-		case "get":
-			const oneContact = await contacts.getContactById(id);
-			return console.log(oneContact);
+			case "get":
+				const oneContact = await contacts.getContactById(id);
+				return console.log(oneContact);
 
-		case "add":
-			const newContact = await contacts.addContact({ name, email, phone });
-			return console.log(newContact);
+			case "add":
+				const newContact = await contacts.addContact({ name, email, phone });
+				return console.log(newContact);
 
-		case "remove":
-			const removeContactById = await contacts.removeContact();
-			return console.log(removeContactById);
+			case "remove":
+				const removeContactById = await contacts.removeContact();
+				return console.log(removeContactById);
 
-		default:
-			console.warn("\x1B[31m Unknown action type!");
+			default:
+				console.warn("\x1B[31m Unknown action type!");
+		}
+	} catch (error) {
+		console.log(error.message);
 	}
 };
 
